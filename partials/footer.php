90ae40a511d6a7f288d2815aa7ee8909df15a819
Вадим <vadym.harasym.kb.2024@lpnu.ua>
2025-12-17 20:58:49 +0200

    Create footer.php

---
+++ partials/footer.php
@@ -0,0 +1,12 @@
+</main>
+
+<footer class="footer">
+  <div class="container">
+    <div class="footer__inner">
+      <span>© <?php echo date('Y'); ?> <?php echo htmlspecialchars(APP_NAME); ?></span>
+      <p align="right"><span> Inst: @property_agency </span></p>
+    </div>
+  </div>
+</footer>
+</body>
+</html>
